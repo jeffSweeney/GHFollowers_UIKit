3fffc6676f6511e44a60b4326525f7aaf00eee84
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-12-31 15:40:36 -0500

    Build basic app structure with assets and omitting storyboards

---
+++ GHFollowers_UIKit/GHFollowers_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers_UIKit
+//
+//  Created by Jeffrey Sweeney on 12/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
