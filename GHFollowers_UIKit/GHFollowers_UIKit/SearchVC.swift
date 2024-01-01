95fdee8a89fc892a0c5d67431df058c6c2bec1f1
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-01-01 13:48:40 -0500

    Implement the SearchVC

--- GHFollowers_UIKit/GHFollowers_UIKit/SearchVC.swift
+++ GHFollowers_UIKit/GHFollowers_UIKit/SearchVC.swift
@@ -8,9 +8,60 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    let logoImageView = UIImageView()
+    let usernameTF = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+        configureTFView()
+        configureCallToActionButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Need to hide this every time viewDidAppear instead of viewDidLoad due to the ability to come "back" to this screen (i.e. view has already been loaded)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    private func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    private func configureTFView() {
+        view.addSubview(usernameTF)
+        usernameTF.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            usernameTF.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTF.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTF.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTF.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+        callToActionButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 }
