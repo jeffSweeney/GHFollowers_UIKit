cad550cd7f0222a6e1533beedaf1c91ca102f614
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-01-02 08:14:12 -0500

    Define our models

---
+++ GHFollowers_UIKit/GHFollowers_UIKit/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers_UIKit
+//
+//  Created by Jeffrey Sweeney on 1/2/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
