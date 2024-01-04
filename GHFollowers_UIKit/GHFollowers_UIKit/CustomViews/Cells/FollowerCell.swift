e1c67f471a80233527f12516b7f83773d4bd2a2c
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-01-04 17:46:39 -0500

    Build the FollowerCell for the collection view

---
+++ GHFollowers_UIKit/GHFollowers_UIKit/CustomViews/Cells/FollowerCell.swift
@@ -0,0 +1,49 @@
+//
+//  FollowerCell.swift
+//  GHFollowers_UIKit
+//
+//  Created by Jeffrey Sweeney on 1/4/24.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseId = "FollowerCell"
+    
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            // Avatar Image Constraints
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor), // Width determined with the anchors above
+            
+            // username Label Constraints
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20), // Font is 16 - give a little extra room for the "hanging" letters
+        ])
+    }
+}
