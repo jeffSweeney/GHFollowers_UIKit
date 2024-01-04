26f3a29a465967f118a121f4bac19e9d97aca0dd
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-01-04 18:03:45 -0500

    Add the foundation of our collection view in the FollowerListVC

--- GHFollowers_UIKit/GHFollowers_UIKit/Screens/FollowerListVC.swift
+++ GHFollowers_UIKit/GHFollowers_UIKit/Screens/FollowerListVC.swift
@@ -9,14 +9,38 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     var username: String!
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+        configureVC()
+        getFollowers()
+    }
+    
+    // Required for edge case bug where you partially SWIPE back but don't complete the action and bounce back to this screen.
+    // - SearchVC (even though we don't fully land on the screen) runs its viewWillAppear and hides the nav bar
+    // - This VC now needs to unhide it as it reappears since viewDidLoad is only ran once on first landing
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
+        navigationController?.setNavigationBarHidden(false, animated: true) // Partial swipe makes animation snappy without this
+    }
+    
+    private func configureVC() {
         view.backgroundColor = .systemBackground
         navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
-        
+    }
+    
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink // TODO: Debug - will update later
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseId)
+    }
+    
+    private func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             switch result {
             case .success(let followers):
@@ -27,14 +51,5 @@ class FollowerListVC: UIViewController {
                 return
             }
         }
-    }
-    
-    // Required for edge case bug where you partially SWIPE back but don't complete the action and bounce back to this screen.
-    // - SearchVC (even though we don't fully land on the screen) runs its viewWillAppear and hides the nav bar
-    // - This VC now needs to unhide it as it reappears since viewDidLoad is only ran once on first landing
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        navigationController?.setNavigationBarHidden(false, animated: true) // Partial swipe makes animation snappy without this
     }
 }
