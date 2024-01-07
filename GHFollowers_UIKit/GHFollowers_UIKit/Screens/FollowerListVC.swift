4cfc2a8063d35bb8bd57b939920c5857a022fd16
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-01-07 13:58:42 -0500

    Display the GFEmptyStateView where applicable

--- GHFollowers_UIKit/GHFollowers_UIKit/Screens/FollowerListVC.swift
+++ GHFollowers_UIKit/GHFollowers_UIKit/Screens/FollowerListVC.swift
@@ -63,6 +63,13 @@ class FollowerListVC: UIViewController {
                 if followers.count < 100 { self.hasMoreFollowers = false } // Prevents future API calls
                 
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them 😃!"
+                    DispatchQueue.main.async { self.showEmptyStateView(with: message, in: self.view) }
+                    return
+                }
+                
                 self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened!", message: error.rawValue, buttonTitle: "Ok")
